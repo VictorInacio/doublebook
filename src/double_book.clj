201986ed744160698760b17d1cdf7ad9ed844c57
Victor Inacio <victorinacio@gmail.com>
2024-07-08 19:05:17 -0300

    implement functions and test cases

--- src/double_book.clj
+++ src/double_book.clj
@@ -6,16 +6,7 @@
     (or (and (<= start1 start2) (> end1 start2))
       (and (<= start2 start1) (> end2 start1)))))
 
-#_(defn find-overlapping-pairs
-  "Find all overlapping pairs of events"
-  [events]
-  (let [events-count (count events)                         ;; Used for pairs assembly
-        pairs (mapcat (fn [i]                               ;; Creates all combinations of event pairs
-                        (map (fn [j]
-                               [(nth events i) (nth events j)])
-                          (range (inc i) events-count)))
-                (range events-count))]
-    (filter #(apply overlap? %) pairs)))
+
 
 (defn find-overlapping-pairs [events]
   (let [pairs (for [i (range (count events))
